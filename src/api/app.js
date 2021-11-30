ff26c4a3b93cfef77c82e1d0a75fb273781371b5
Roberval Filho <rslfilho@gmail.com>
2021-11-29 21:49:55 -0300

    Requisito 10

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const root = require('../controllers/root');
 const { error } = require('../middlewares');
@@ -7,6 +8,8 @@ const { error } = require('../middlewares');
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, '../uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
